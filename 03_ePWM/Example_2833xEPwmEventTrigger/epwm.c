1e8bf949b2dc4b085314be871420e478134f8b08
greatak007007 <greatak007007@gmail.com>
2026-07-09 17:26:44 +0800

    epwm1 event trigger

---
+++ 03_ePWM/Example_2833xEPwmEventTrigger/epwm.c
@@ -0,0 +1,65 @@
+#include "DSP28x_Project.h"     // Device Headerfile and Examples Include File
+#include "DSP2833x_EPwm_defines.h"
+
+void InitEPwm1Simple(void)
+{
+    //============================================
+    // time-base
+    //============================================
+    EPwm1Regs.TBCTL.bit.CTRMODE = TB_COUNT_UPDOWN;
+    EPwm1Regs.TBPRD = 1000;
+    EPwm1Regs.TBCTR = 0;
+
+    //undivided frequency
+    EPwm1Regs.TBCTL.bit.HSPCLKDIV = TB_DIV1;
+    EPwm1Regs.TBCTL.bit.CLKDIV =TB_DIV1;
+
+    //============================================
+    // compare
+    //============================================
+    //shawdow load on zero
+    EPwm1Regs.CMPCTL.bit.SHDWAMODE = CC_SHADOW;
+    EPwm1Regs.CMPCTL.bit.SHDWBMODE = CC_SHADOW;
+    EPwm1Regs.CMPCTL.bit.LOADAMODE = CC_CTR_ZERO;
+    EPwm1Regs.CMPCTL.bit.LOADBMODE = CC_CTR_ZERO;
+
+    EPwm1Regs.CMPA.half.CMPA = 400;
+
+    //============================================
+    // action qualifier
+    //============================================
+    EPwm1Regs.AQCTLA.bit.CAU = AQ_CLEAR;
+    EPwm1Regs.AQCTLA.bit.CAD = AQ_SET;
+    //============================================
+    // dead-band
+    //============================================
+    EPwm1Regs.DBCTL.bit.OUT_MODE = DB_FULL_ENABLE;
+    EPwm1Regs.DBCTL.bit.POLSEL = DB_ACTV_HIC;
+    EPwm1Regs.DBCTL.bit.IN_MODE = DBA_ALL;
+
+    EPwm1Regs.DBRED = 200;
+    EPwm1Regs.DBFED = 200;
+    //============================================
+    // event-trigger
+    //============================================
+    EPwm1Regs.ETSEL.bit.INTSEL = ET_CTR_ZERO;//when tbctr=0, produce event
+    EPwm1Regs.ETSEL.bit.INTEN = 1;
+    EPwm1Regs.ETPS.bit.INTPRD = ET_1ST;//when meet every event, produce an interrupt
+}
+
+void InitEPwm6Simple(void)
+{
+    EPwm6Regs.TBCTL.bit.CTRMODE = TB_COUNT_UP;
+    EPwm6Regs.TBPRD = 1000;
+    EPwm6Regs.TBCTR = 0;
+
+    //undivided frequency
+    EPwm6Regs.TBCTL.bit.HSPCLKDIV = TB_DIV1;
+    EPwm6Regs.TBCTL.bit.CLKDIV =TB_DIV1;
+
+    EPwm6Regs.CMPA.half.CMPA = 500;
+
+    //action
+    EPwm6Regs.AQCTLA.bit.ZRO = AQ_SET;
+    EPwm6Regs.AQCTLA.bit.CAU = AQ_CLEAR;
+}
